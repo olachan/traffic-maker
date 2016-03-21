6b19bbe15ee46dc6b0ff48e496738869ca22f1db
wujuguang <1154545932@qq.com>
2016-03-21 23:04:12 +0800

    增数据从文件读取形式.

---
+++ db/base.py
@@ -0,0 +1,43 @@
+#!usr/bin/env python
+# coding: utf-8
+
+
+class Process(object):
+    """数据处理, 从指定路径中读所有数据返回其所有行.
+    """
+
+    def __init__(self, parent_path):
+        """初始化操作路径, 操作命令.
+
+            :parameter parent_path: 操作路径
+        """
+
+        self._directory = parent_path
+        self._all_lines = []
+
+    def read_lines(self, file_path):
+        pass
+
+    def process_target_path(self, target_path):
+        pass
+
+    def run_work(self):
+        """对指定的操作路径, 执行指定的操作命令.
+        """
+
+        if isinstance(self._directory, basestring):
+            self.process_target_path(self._directory)
+        elif isinstance(self._directory, (tuple, list)):
+            for path in self._directory:
+                self.process_target_path(path)
+        else:
+            pass
+
+        print("Ok, Files are done!\r")
+
+    def __call__(self):
+        self.run_work()
+
+        if self._all_lines:
+            # 去重
+            return list(set(self._all_lines))
---
+++ db/files.py
@@ -0,0 +1,75 @@
+#!usr/bin/env python
+# coding: utf-8
+
+import os
+import xml.dom.minidom as xml_dom
+
+from base import Process
+
+
+class FileProcess(Process):
+    """文件处理, 从指定目录中读所有文件返回其所有行.
+    """
+
+    def process_target_path(self, target_path):
+        """对指定目录执行操作.
+
+            :param target_path: 指定目录
+        """
+
+        # 判断路径是否存在
+        if not os.path.exists(target_path):
+            print("Directory does not exist!")
+            return
+
+        for i in os.listdir(target_path):
+            sub_path = os.path.join(target_path, i)
+            if os.path.isdir(sub_path):
+                self.process_target_path(sub_path)
+            elif os.path.isfile(sub_path):
+                self.read_lines(sub_path)
+            else:
+                pass
+
+
+class TXTProcess(FileProcess):
+    """适用于行分隔的网址.
+    """
+
+    def read_lines(self, file_path):
+        """读取无格式文本文件.
+
+            :parameter file_path: 文件
+        """
+
+        if os.path.exists(file_path) and os.path.isfile(file_path):
+            with open(file_path, 'rb') as f:
+                lines = f.readlines()
+                if lines:
+                    self._all_lines.extend(lines)
+
+
+class XMLProcess(FileProcess):
+    """适用于站点地图(sitemap.xml).
+    """
+
+    def read_lines(self, file_path):
+        """读取有格式文本文件.
+
+            :parameter file_path: 文件
+        """
+
+        node_flag = 'loc'  # 节点标识
+
+        if os.path.exists(file_path) and os.path.isfile(file_path):
+            try:
+                xml_file = xml_dom.parse(file_path)
+            except Exception, ex:
+                print(ex)
+                lines = None
+            else:
+                items = xml_file.documentElement.getElementsByTagName(node_flag)
+                lines = [item.firstChild.data for item in items]
+
+            if lines:
+                self._all_lines.extend(lines)
